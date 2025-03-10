a08d1057dfb1729545e53af86b92055765ccb14f
JeffGreenlee42 <banjo1@gmail.com>
2025-03-09 18:26:05 -0700

    Now at Acceleration of Chapt 4

--- Learning Physics (book)/Chapter 4 - Describing Motion/motion.hs
+++ Learning Physics (book)/Chapter 4 - Describing Motion/motion.hs
@@ -9,7 +9,26 @@ type PositionFunction = Time -> Position
 type VelocityFunction = Time -> Velocity
 
 
-averageVelocity :: Time -> Time -> PositionFunction -> Velocity
+-- averageVelocity :: Time -> Time -> PositionFunction -> Velocity
 
-averageVelocity2 :: Time -> TimeInterval -> PositionFunction -> Velocity
-averageVelocity2 t dt x = (x (t + dt/2)) - x (t - dt/2) / dt
+-- averageVelocity2 :: Time -> TimeInterval -> PositionFunction -> Velocity
+-- averageVelocity2 t dt x = (x (t + dt/2)) - x (t - dt/2) / dt
+
+type Derivative = (R -> R) -> R -> R
+
+derivative :: R -> Derivative
+derivative dt x t = (x (t + dt/2) - x (t - dt/2)) / dt
+
+carPosition :: Time -> Position
+carPosition t = cos t
+
+carVelocity :: Time -> Velocity
+carVelocity = derivative 0.01 carPosition
+
+velFromPos :: R                         -- dt
+            -> (Time -> Position)       -- position function
+            -> (Time -> Velocity)       -- velocity function
+velFromPos = derivative
+
+positionCV :: Position -> Velocity -> Time -> Position
+positionCV x0 v0 t = v0 * t + x0
